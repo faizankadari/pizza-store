469f9de3990a507eb86ba19a686cf3d3c140b862
faizankadari <faizankadari079@gmail.com>
2025-04-02 14:01:50 +0530

    Initial Project setup completed

---
+++ pizza-store/src/main/java/com/example/pizzastore/PizzastoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzastoreApplication.class, args);
+	}
+
+}
