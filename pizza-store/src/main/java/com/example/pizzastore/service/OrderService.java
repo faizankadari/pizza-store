9e184598dd753652ec626f185c03e5a57575c155
faizankadari <faizankadari079@gmail.com>
2025-04-02 20:06:05 +0530

    Service layer completed

---
+++ pizza-store/src/main/java/com/example/pizzastore/service/OrderService.java
@@ -0,0 +1,74 @@
+package com.example.pizzastore.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.pizzastore.model.Order;
+import com.example.pizzastore.model.OrderDetails;
+import com.example.pizzastore.repository.OrderDetailsRepository;
+import com.example.pizzastore.repository.OrderRepository;
+import com.example.pizzastore.repository.PizzaRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	@Autowired
+	private OrderDetailsRepository orderDetailsRepository;
+
+	@Autowired
+	private PizzaRepository pizzaRepository;
+
+	OrderService(OrderDetailsRepository orderDetailsRepository) {
+		this.orderDetailsRepository = orderDetailsRepository;
+	}
+
+	public Mono<OrderDetails> createOrder(Order order) {
+		// Validate each pizza ID in the order and fetch corresponding Pizza objects
+		return Flux.fromIterable(order.getPizzasID()).flatMap(pizzaRepository::findById).collectList()
+				.flatMap(pizzas -> {
+					if (pizzas.size() != order.getPizzasID().size()) {
+						return Mono.error(new RuntimeException("One or more pizzas not found"));
+					}
+
+					// Create OrderDetails object
+					OrderDetails orderDetails = new OrderDetails();
+					orderDetails.setOrderDetailId(order.getOrderId()); // Map Order ID to OrderDetails ID
+					orderDetails.setPizzas(pizzas); // Set list of valid Pizza objects
+					orderDetails.setPizzasID(order.getPizzasID()); // Set list of pizza IDs
+					orderDetails.setStatus("Preparing"); // Initial status
+					orderDetails.setTimestamp(LocalDateTime.now()); // Current timestamp
+
+					orderRepository.save(order);
+					return orderDetailsRepository.save(orderDetails); // Save and return OrderDetails
+				});
+	}
+
+	public Mono<OrderDetails> getOrderById(String id) {
+		return orderDetailsRepository.findById(id);
+	}
+
+	public Flux<OrderDetails> getAllOrders() {
+		return orderDetailsRepository.findAll();
+	}
+
+	public Mono<OrderDetails> updateOrderStatus(String id) {
+		return orderDetailsRepository.findById(id).flatMap(order -> {
+			order.setStatus("Delivered");
+
+			return orderDetailsRepository.save(order);
+		});
+	}
+
+	public Mono<Void> deleteOrder(String id) {
+		orderRepository.findById(id).flatMap(existingOrder -> orderRepository.deleteById(id));
+		return orderDetailsRepository.findById(id).flatMap(existingOrder -> orderDetailsRepository.deleteById(id));
+	}
+}
---
+++ pizza-store/src/main/java/com/example/pizzastore/service/PizzaService.java
@@ -0,0 +1,53 @@
+package com.example.pizzastore.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.pizzastore.model.Pizza;
+import com.example.pizzastore.repository.PizzaRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class PizzaService {
+
+    @Autowired
+    private PizzaRepository pizzaRepository;
+
+    public Mono<Pizza> createPizza(Pizza pizza) {
+        return pizzaRepository.save(pizza);
+    }
+
+    public Mono<Pizza> getPizzaById(Integer id) {
+        return pizzaRepository.findById(id);
+    }
+
+    public Flux<Pizza> getAllPizzas() {
+        return pizzaRepository.findAll();
+    }
+
+    public Flux<Pizza> getPizzaByName(String name) {
+        return pizzaRepository.findByName(name);
+    }
+
+    public Mono<Pizza> updatePizza(Integer id, Pizza pizza) {
+        return pizzaRepository.findById(id)
+                .flatMap(existingPizza -> {
+                    pizza.setId(pizza.getId());
+                    pizza.setName(pizza.getName());
+                    pizza.setPrice(pizza.getPrice());
+                    pizza.setSize(pizza.getSize());
+                    pizza.setDescription(pizza.getDescription());
+                    pizza.setToppings(pizza.getToppings());
+                    return pizzaRepository.save(pizza);
+                });
+    }
+
+    public Mono<Void> deletePizza(Integer id) {
+        return pizzaRepository.findById(id)
+                .flatMap(existingPizza -> pizzaRepository.deleteById(id));
+    }
+}
+
