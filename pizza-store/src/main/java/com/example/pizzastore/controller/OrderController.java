d2f3237e72acf44bac339de80b875d4e6660d1ba
faizankadari <faizankadari079@gmail.com>
2025-04-03 12:18:51 +0530

    Controller layer completed

---
+++ pizza-store/src/main/java/com/example/pizzastore/controller/OrderController.java
@@ -0,0 +1,68 @@
+package com.example.pizzastore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pizzastore.model.Order;
+import com.example.pizzastore.model.OrderDetails;
+import com.example.pizzastore.service.OrderService;
+
+import jakarta.validation.Valid;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+	@Autowired
+	private OrderService orderService;
+
+	// Create a new order
+	@PostMapping
+	@ResponseStatus(HttpStatus.OK)
+	public Mono<OrderDetails> createOrder(@Valid @RequestBody Order order) {
+		System.out.println("Controller");
+		return orderService.createOrder(order);
+	}
+
+	// Get an order by ID
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Mono<ResponseEntity<OrderDetails>> getOrderById(@PathVariable String id) {
+		return orderService.getOrderById(id).map(order -> ResponseEntity.ok(order))
+				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+
+	// Get all orders
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public Flux<OrderDetails> getAllOrders() {
+		return orderService.getAllOrders();
+	}
+
+	// Update the status of an existing order
+	@PutMapping("/{id}/status")
+	@ResponseStatus(HttpStatus.OK)
+	public Mono<ResponseEntity<OrderDetails>> updateOrderStatus(@PathVariable String id) {
+		return orderService.updateOrderStatus(id).map(updatedOrder -> ResponseEntity.ok(updatedOrder))
+				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+
+	// Delete an order by ID
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Mono<Void> deleteOrder(@PathVariable String id) {
+		return orderService.deleteOrder(id).switchIfEmpty(Mono.error(new RuntimeException("Pizza not found")));
+	}
+}
---
+++ pizza-store/src/main/java/com/example/pizzastore/controller/PizzaController.java
@@ -0,0 +1,71 @@
+package com.example.pizzastore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pizzastore.model.Pizza;
+import com.example.pizzastore.service.PizzaService;
+
+import jakarta.validation.Valid;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/pizzas")
+public class PizzaController {
+
+	@Autowired
+	private PizzaService pizzaService;
+
+	// Create a new pizza
+	@PostMapping
+	public ResponseEntity<Mono<Pizza>> createPizza(@Valid @RequestBody Pizza pizza) {
+		return new ResponseEntity<>(pizzaService.createPizza(pizza), HttpStatus.OK);
+	}
+
+	// Get a pizza by ID
+	@GetMapping("/{id}")
+	public Mono<ResponseEntity<Pizza>> getPizzaById(@PathVariable Integer id) {
+		return pizzaService.getPizzaById(id).map(pizza -> ResponseEntity.ok(pizza))
+				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+
+	// Get all pizzas
+	@GetMapping
+	public Flux<Pizza> getAllPizzas() {
+		return pizzaService.getAllPizzas();
+	}
+
+	// Get pizzas by name
+	@GetMapping("/search")
+	public Flux<Pizza> getPizzaByName(@RequestParam String name) {
+		return pizzaService.getPizzaByName(name);
+	}
+
+	// Update an existing pizza
+	@PutMapping("/{id}")
+	public Mono<ResponseEntity<Pizza>> updatePizza(@PathVariable Integer id, @Valid @RequestBody Pizza pizza) {
+		return pizzaService.updatePizza(id, pizza).map(updatedPizza -> ResponseEntity.ok(updatedPizza))
+				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+
+    // Delete Pizza by ID
+	@DeleteMapping("/{id}")
+	public Mono<ResponseEntity<String>> deletePizza(@PathVariable Integer id) {
+		return pizzaService.deletePizza(id).
+				then(Mono.just(ResponseEntity.ok("Pizza Deleted Successfully")))
+				.switchIfEmpty(Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND)
+						.body("Pizza not found")));
+	}
+
+}
